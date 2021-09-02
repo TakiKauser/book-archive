5519b76c1808358ed3cde3645a968f2bd8398b94
TakiKauser <takikauserbadhon@gmail.com>
2021-09-02 13:46:29 +0600

    styles and comments

--- js/app.js
+++ js/app.js
@@ -1,18 +1,25 @@
+// spinner display toggle function
 const spinnerDisplayToggle = displayStyle => {
     document.getElementById("spinner").style.display = displayStyle;
 };
+// search result display toggle function
 const searchResultsDisplayToggle = () => {
     document.getElementById("found-items").innerText = "";
     document.getElementById("search-result").textContent = "";
 };
+// function to clear error message area
 const errorMessageToggle = () => {
     document.getElementById("error-messages").innerText = "";
 };
+// display error message function
 const errorMessage = message => {
     document.getElementById("error-messages").innerText = message;
 };
+// set spinner hidden initially 
 spinnerDisplayToggle("none");
-const searchBook = () => {
+// onclick event handler function
+document.getElementById("button-search").addEventListener("click", searchBook = () => {
+    // get input text
     const searchTextInput = document.getElementById("search-field");
     const searchText = searchTextInput.value;
     if (searchTextInput.value === "") {
@@ -26,9 +33,11 @@ const searchBook = () => {
         errorMessageToggle();
         loadJsonData(searchText);
     }
+    // clear input field
     searchTextInput.value = "";
-};
+});
 const loadJsonData = async searchText => {
+    // fetch json data from api
     const url = `https://openlibrary.org/search.json?q=${searchText}`;
     const response = await fetch(url);
     const jsonData = await response.json();
@@ -42,25 +51,27 @@ const loadJsonData = async searchText => {
         displayRequiredBooks(jsonData.docs); // booksLists -> docs
     }
 };
+// function to display found items quantity
 const displayFoundItemsQuantity = itemsCount => {
     const foundItems = document.getElementById("found-items");
     foundItems.innerText = `Books Found: ${itemsCount}`;
     spinnerDisplayToggle("none");
 };
+// function to display required search result
 const displayRequiredBooks = books => {
     spinnerDisplayToggle("none");
     const searchResult = document.getElementById("search-result");
     books.forEach(book => {
         const div = document.createElement("div");
-        div.classList.add("col");
+        div.classList.add("col", "shadow", "p-3", "border", "rounded");
         div.innerHTML = `
         <div class="card h-100">
             <img height="300px" src="https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg" class="card-img-top" alt="...">
             <div class="card-body">
-                <h4 class="card-title">Title: ${book.title}</h4>
-                <h5 class="card-title">Author: ${book.author_name ? book.author_name : "N/A"}</h5>
-                <h5 class="card-title">Publisher: ${book.publisher ? book.publisher : "N/A"}</h5>
-                <h6 class="card-title">First Published: ${book.first_publish_year ? book.first_publish_year : "N/A"}</h6>
+                <h4 class="card-title"><span class="text-secondary">Title:</span> <span class="fw-bold">${book.title}</span></h4>
+                <h5 class="card-title text-secondary">Author: <span class="fw-bold fst-italic">${book.author_name ? book.author_name[0] : "N/A"}</span></h5>
+                <h5 class="card-title"><span class="text-secondary">Publisher:</span> ${book.publisher ? book.publisher[0] : "N/A"}</h5>
+                <h6 class="card-title"><span class="text-secondary">First Published:</span> ${book.first_publish_year ? book.first_publish_year : "N/A"}</h6>
             </div>
         </div>
         `;
